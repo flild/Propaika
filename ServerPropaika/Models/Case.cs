c03f2e6acfc1fa7eed99823e1694aed96db264c5
flild <flildman@yandex.ru>
2025-08-27 00:34:14 +0300

    туда сюда, старт

---
+++ ServerPropaika/Models/Case.cs
@@ -0,0 +1,14 @@
+﻿namespace ServerPropaika.Models
+{
+    public class Case
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public string? BeforeImage { get; set; }
+        public string? AfterImage { get; set; }
+        public decimal? Price { get; set; }
+        public bool IsPublished { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
